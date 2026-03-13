a0780de353115ac9460378339777e7ff990fd333
Chenzy <2838397338@qq.com>
2026-03-13 16:04:09 +0800

    init 初始化

---
+++ src/main/java/com/aiProject/AIProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AIProjectApplication.class, args);
+    }
+
+}
